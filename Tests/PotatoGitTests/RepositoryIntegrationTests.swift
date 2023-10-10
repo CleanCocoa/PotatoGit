6b0fa13bce1b16fc59f5752adb7460f3cba3841c
Christian Tietze <me@christiantietze.de>
2023-10-10 10:19:24 +0200

    add failing integration test

---
+++ Tests/PotatoGitTests/RepositoryIntegrationTests.swift
@@ -0,0 +1,22 @@
+import XCTest
+@testable import PotatoGit
+
+final class RepositoryIntegrationTests: XCTestCase {
+    let localURL = FileManager.default.temporaryDirectory
+        .appendingPathComponent("de.christiantietze.potatogit")
+        .appendingPathComponent("tests")
+    let remoteURL = URL(string: "https://github.com/CleanCocoa/PotatoGit")
+
+    override func setUpWithError() throws {
+        if FileManager.default.fileExists(atPath: localURL.path(percentEncoded: false)) {
+            try FileManager.default.removeItem(at: localURL)
+        }
+        try FileManager.default.createDirectory(at: localURL, withIntermediateDirectories: true)
+    }
+
+    func testCloningPotatoGitRepository() throws {
+        XCTAssertFalse(try Repository.exists(at: localURL).get())
+
+        XCTAssertTrue(try Repository.exists(at: localURL).get())
+    }
+}
