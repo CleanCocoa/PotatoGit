571a120e97a54bbb4e65d820c0d0f6dacc3f5639
Christian Tietze <me@christiantietze.de>
2023-10-10 11:44:40 +0200

    limit package to macOS 10.13+

--- Package.swift
+++ Package.swift
@@ -4,6 +4,9 @@ import PackageDescription
 
 let package = Package(
     name: "PotatoGit",
+    platforms: [
+        .macOS("10.13"),
+    ],
     products: [
         .library(
             name: "PotatoGit",
